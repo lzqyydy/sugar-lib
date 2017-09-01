ca3444060ceae515f17e48f0b2f5430f4f55932d
lzq <zq.liu@neoclub.cn>
2017-09-01 17:48:50 +0800

    u

---
+++ folder.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="{'folder': !inline, 'folder-inline': inline}">
+    <div :class="{'collapsed': collapse}" @mousedown="op" @touchstart="op">{{name}}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapse: true
+    };
+  },
+  props: ['inline', 'name'],
+  methods: {
+    op(){
+      this.collapse = !this.collapse
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.collapsed ~ *{
+  transition: height 0.5s ease-out;
+  height: 0;
+  overflow: hidden;
+  /*display: none;*/
+}
+</style>
